cf47caddd8ecbfe31f02a5353a83700f22e256c2
Takaya-Yamazato <yamazato@ilas.nagoya-u.ac.jp>
2021-06-24 17:53:46 +0900

    featured post を削除

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -13,7 +13,7 @@ class BlogRoll extends React.Component {
         {posts &&
           posts.map(({ node: post }) => (
             <div className="column post is-4" key={post.id}>
-              <article className={`blog-list-item tile is-child box notification ${post.frontmatter.featuredpost ? "is-featured" : ""}`}>
+              <article className={`blog-list-item tile is-child box notification `}>
                 {post.frontmatter.featuredimage ? (
                   <div className="column is-12 featured-content ">
                     <PreviewCompatibleImage
@@ -73,7 +73,6 @@ export default function BlogRollQuery() {
                   templateKey
                   date(formatString: "MMMM DD, YYYY")
                   description
-                  featuredpost
                   featuredimage {
                     childImageSharp {
                       gatsbyImageData(width: 240, quality: 100, layout: CONSTRAINED)
