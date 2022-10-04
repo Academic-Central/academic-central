32e1c0ebe2c057cdb6f94b07d3dd1a77c3cb81aa
Takaya-Yamazato <yamazato@ilas.nagoya-u.ac.jp>
2022-10-04 16:58:18 +0900

    Delete null tag

--- gatsby-node.js
+++ gatsby-node.js
@@ -86,6 +86,11 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   // Eliminate duplicate tags
   tags = _.uniq(tags);
 
+  // Delete null tag
+  tags = tags.filter(function (e) {
+    return e != null
+  })
+  
   // Make tag pages
   tags.forEach(tag => {
     const tagPath = `/tags/${_.kebabCase(tag)}/`;
